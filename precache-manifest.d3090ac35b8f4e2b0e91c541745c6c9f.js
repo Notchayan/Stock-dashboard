5959005db83a0defbe7077e6b3124269c5daf856
Notchayan <kumawatchayan4@gmail.com>
2023-09-05 02:05:53 +0530

    Updates

---
+++ precache-manifest.d3090ac35b8f4e2b0e91c541745c6c9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e425b41584db92d3def85c98eed58c30",
+    "url": "/Stock-dashboard/index.html"
+  },
+  {
+    "revision": "f198836161efc5e92ee0",
+    "url": "/Stock-dashboard/static/css/main.8d275919.chunk.css"
+  },
+  {
+    "revision": "c38b342d5f4c8d111239",
+    "url": "/Stock-dashboard/static/js/2.5ecf9404.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/Stock-dashboard/static/js/2.5ecf9404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f198836161efc5e92ee0",
+    "url": "/Stock-dashboard/static/js/main.87d6ca1b.chunk.js"
+  },
+  {
+    "revision": "56a0a3987c8921c87b67",
+    "url": "/Stock-dashboard/static/js/runtime-main.37d3c3d3.js"
+  }
+]);
