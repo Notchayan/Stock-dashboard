f090d4cb6ada82a9f022be4050c45ad1db75e57c
Notchayan <130827975+Notchayan@users.noreply.github.com>
2023-09-05 03:13:10 +0530

    Update TableDataCard.js

--- src/components/TableDataCard.js
+++ src/components/TableDataCard.js
@@ -10,16 +10,13 @@ class TableDataCard extends React.Component {
   render() {
     let tableDataDOM = '';
 
-    // @dom: show all values in the table
     tableDataDOM = this.props.tableData.map((table, index) => {
-      // @condition: check if there is a viable response from the API call (e.g. AAAA doesn't exist)
       if (table !== 'Symbol not supported') {
         return (
           <tr key={index}>
             <th scope="row">{this.props.graphData[index].stockValue}</th>
             <td>{new Date(table.t * 1000).toLocaleDateString()}</td>
 
-            {/* Open Price */}
             <td
               className={`${
                 this.state.currency + table.o.toFixed(2) >
@@ -31,7 +28,6 @@ class TableDataCard extends React.Component {
               {this.state.currency + table.o.toFixed(2)}
             </td>
 
-            {/* Low Price */}
             <td
               className={`${
                 this.state.currency + table.l.toFixed(2) >
@@ -43,7 +39,6 @@ class TableDataCard extends React.Component {
               {this.state.currency + table.l.toFixed(2)}
             </td>
 
-            {/* High Price */}
             <td
               className={`${
                 this.state.currency + table.h.toFixed(2) >
@@ -55,7 +50,6 @@ class TableDataCard extends React.Component {
               {this.state.currency + table.h.toFixed(2)}
             </td>
 
-            {/* Previous Close Price */}
             <td
               className={`${
                 this.state.currency + table.c.toFixed(2) <
@@ -67,7 +61,6 @@ class TableDataCard extends React.Component {
               {this.state.currency + table.pc.toFixed(2)}
             </td>
 
-            {/* Close Price */}
             <td
               className={`${
                 this.state.currency + table.c.toFixed(2) >
@@ -79,7 +72,6 @@ class TableDataCard extends React.Component {
               {this.state.currency + table.c.toFixed(2)}
             </td>
 
-            {/* PC/C percentage */}
             <td
               className={`${
                 this.state.currency + table.c.toFixed(2) >
@@ -96,7 +88,7 @@ class TableDataCard extends React.Component {
           </tr>
         );
       }
-      return null; // Skip rendering for unsupported symbols
+      return null; 
     });
 
     return (
